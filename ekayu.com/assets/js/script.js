c66a9acc89a44b196ace44a20fc00e01beb7d4f8
shiyanigunjan710 <gunjanshiyani123456789@gmail.com>
2023-03-18 18:06:15 +0530

    incr (Ekayu.com)

---
+++ ekayu.com/assets/js/script.js
@@ -0,0 +1,9 @@
+AOS.init();
+// anime({
+//     targets: '.bg-image',
+//     scale: [
+//         { value: .1, easing: 'easeOutSine', duration: 500 },
+//         { value: 1, easing: 'easeInOutQuad', duration: 1000 }
+//     ],
+//     delay: anime.stagger(0, { grid: [14, 5], from: 'center' })
+// });
